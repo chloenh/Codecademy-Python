070690f1ed2dd0d42fca114dc4ceb04dce0e3154
cnastyah <45791822+cnastyah@users.noreply.github.com>
2019-01-16 12:06:47 +0100

    Create list-slicing-solution.py

---
+++ list-slicing-solution.py
@@ -0,0 +1,13 @@
+#Tasks: On line 7, create a list called middle containing only the two middle items from suitcase.
+#On line 10, create a list called last made up only of the last two items from suitcase.
+
+suitcase = ["sunglasses", "hat", "passport", "laptop", "suit", "shoes"]
+
+# The first and second items (index zero and one)
+first = suitcase[0:2]
+
+# Third and fourth items (index two and three)
+middle = suitcase[2:4]
+
+# The last two items (index four and five)
+last =  suitcase[4:6]
