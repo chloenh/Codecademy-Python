9505190d84cb1bf647d8653da17c4088fb9ee8fd
cnastyah <45791822+cnastyah@users.noreply.github.com>
2019-01-17 22:12:59 +0100

    Create for-loop-solution.py

---
+++ for-loop-solution.py
@@ -0,0 +1,6 @@
+#Write a statement in the indented part of the for-loop that prints a number equal to 2 * number for every list item.
+
+my_list = [1,9,3,8,5,7]
+
+for number in my_list: 
+  print 2 * number
