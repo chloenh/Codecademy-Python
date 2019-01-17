c5ea9ac9cee87fc93776ec5eb6abdadb09158822
cnastyah <45791822+cnastyah@users.noreply.github.com>
2019-01-17 22:52:28 +0100

    Create dictionary-solution.py

---
+++ dictionary-solution.py
@@ -0,0 +1,11 @@
+#Print the values stored under the 'Sloth' 
+#and 'Burmese Python' keys. Accessing dictionary values by 
+#key is just like accessing list values by index:
+
+# Assigning a dictionary with three key-value pairs to residents:
+residents = {'Puffin' : 104, 'Sloth' : 105, 'Burmese Python' : 106}
+
+print residents['Puffin'] # Prints Puffin's room number
+
+print residents ['Sloth']
+print residents ['Burmese Python']
