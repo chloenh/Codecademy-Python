4a796ff2675069841ae853b6354fafdd5dd00a44
cnastyah <45791822+cnastyah@users.noreply.github.com>
2019-01-17 22:08:53 +0100

    Create maintaining-order-solution.py

---
+++ maintaining-order-solution.py
@@ -0,0 +1,12 @@
+#Use the .index(item) function to find the index of "duck". Assign that result to a variable called duck_index.
+
+#Then .insert(index, item) the string "cobra" at that index.
+
+
+animals = ["aardvark", "badger", "duck", "emu", "fennec fox"]
+duck_index = animals.index("duck")# Use 
+
+animals.insert(duck_index, "cobra")
+
+
+print animals 
