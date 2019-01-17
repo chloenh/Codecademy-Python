0c429fcaad86290b5abb82efe734742be05d503b
cnastyah <45791822+cnastyah@users.noreply.github.com>
2019-01-17 22:56:11 +0100

    Create new-entries-solution.py

---
+++ new-entries-solution.py
@@ -0,0 +1,16 @@
+#Add at least three more key-value pairs to the menu variable, with the 
+#dish name (as a "string") for the key and the price (a float or integer) as the value. 
+
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print menu['Chicken Alfredo']
+
+
+menu['Lasagne'] = 6.90
+menu['Mango Mochi'] = 3
+menu['Doryaki'] = 10.50
+
+
+
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
