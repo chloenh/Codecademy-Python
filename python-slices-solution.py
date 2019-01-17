7f50704d4d469e4c2ba17611dd206d0e8166b65a
cnastyah <45791822+cnastyah@users.noreply.github.com>
2019-01-17 22:05:36 +0100

    Create python-slices-solution.py

---
+++ python-slices-solution.py
@@ -0,0 +1,11 @@
+#Assign to dog a slice of animals from index 3 up until but not including index 6.
+#Assign to frog a slice of animals from index 6 until the end of the string.
+
+
+animals = "catdogfrog"
+
+cat = animals[:3]
+
+dog = animals[3:6]
+
+frog = animals[6:]
