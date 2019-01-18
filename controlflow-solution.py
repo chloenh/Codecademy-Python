01a30c57b9065f123fa140b2e30cd07b3176a641
cnastyah <45791822+cnastyah@users.noreply.github.com>
2019-01-18 10:53:55 +0100

    Create controlflow-solution.py

---
+++ controlflow-solution.py
@@ -0,0 +1,8 @@
+#Task: loop through each item in the list called a.
+#if the number is even, print it out. 
+
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+for number in a:
+  if number % 2 == 0:
+    print number
