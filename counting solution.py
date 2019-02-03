e32d4d09f58515dfd9c1100d38d31afd7957208f
chloenh <45791822+chloenh@users.noreply.github.com>
2019-02-03 12:33:53 +0100

    Create counting solution.py

---
+++ counting solution.py
@@ -0,0 +1,9 @@
+#We don't want the user to see things listed from index 0, since 
+#this looks unnatural. Instead, the items should appear to start 
+#at index 1. Modify the print statement to reflect this behavior
+
+choices = ['pizza', 'pasta', 'salad', 'nachos']
+
+print 'Your choices are:'
+for index, item in enumerate(choices):
+  print index +1, item
